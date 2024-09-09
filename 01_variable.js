2560bb680eb8535dddbd925cff9511dd83cd09e1
Priyanshuchaturvedi67 <priyanshuchaturvedi67@gmail.com>
2024-09-09 13:19:00 +0530

    Add files via upload

---
+++ 01_variable.js
@@ -0,0 +1,11 @@
+const accountid=14553             //const can not change
+let accountemail="pkc@gmail.com"  //let can change
+var accountpassword="12345"       //var  can also change
+accountcity="jaipur"    //we can create variable without write const ,let ,var   
+let accountstate;  
+console.log(accountid)
+console.table([accountid,accountemail,accountpassword,accountcity, accountstate  ])
+/*
+prefer not to use var
+because of issue in  block scope and functional scope
+*/
---
+++ 02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3)//we are using nodejus , not browser
+console.log(3
+    +
+    3
+)//code readability
+
+let name="priyanshu"
+let age =18
+let isloggedin=false
+let state;
+
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+//object
+console.log(typeof "coder");
+console.log(typeof null );
+console.log(typeof undefined);
+//follow "mdn" documaintation
